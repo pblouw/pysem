b40a9e381c4d8b49a81be11b821210e1e0e51bda
pblouw <pblouw@uwaterloo.ca>
2016-05-05 23:35:07 -0400

    PEP8 fixes

--- pysem/streams.py
+++ pysem/streams.py
@@ -1,22 +1,18 @@
-import os 
-import subprocess
+import os
 import re
 import random
 import nltk
-import string
-import sys
 
 tokenizer = nltk.load('tokenizers/punkt/english.pickle')
 
 
 class StreamGenerator(object):
     """
-    Builds a list of streams that stream sentences from random subsets of a 
+    Builds a list of streams that stream sentences from random subsets of a
     Wikipedia dump (the location of which must be given to the constructor).
     """
     def __init__(self, corpuspath):
         self.corpuspath = corpuspath
-
 
     @staticmethod
     def preprocess(article):
@@ -30,7 +26,6 @@ class StreamGenerator(object):
 
         return sen_list
 
-
     def stream(self, files):
         '''Generator that streams sentences from Wikipedia'''
         for file in files:
@@ -39,8 +34,7 @@ class StreamGenerator(object):
                 for article in articles:
                     sen_list = self.preprocess(article)
                     for sen in sen_list:
-                        yield sen    
-
+                        yield sen
 
     def build_streams(self, n_streams, n_files, randomize=True):
         '''Build a set of generators for streaming Wikipedia text'''
@@ -54,7 +48,6 @@ class StreamGenerator(object):
             path_set = random.sample(file_paths, n_files)
             if not randomize:
                 file_paths = file_paths.difference_update(path_set)
-            
             streams.append(self.stream(path_set))
 
         return streams
@@ -64,7 +57,7 @@ if __name__ == '__main__':
 
     corpuspath = '/Users/peterblouw/corpora/wikipedia'
     wikitext = StreamGenerator(corpuspath)
-        
+
     streams = wikitext.build_streams(1, 1)
 
     for stream in streams:
