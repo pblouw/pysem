a8eb3b5b460c09fae365e8b61b43638f5bc7467c
pblouw <pblouw@gmail.com>
2016-05-18 14:27:31 -0500

    Update SNLI neural net classifier

--- pysem/sandbox.py
+++ pysem/sandbox.py
@@ -2,6 +2,8 @@ import random
 import platform
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 from sklearn.feature_extraction.text import CountVectorizer
 from handlers import SNLI
 
@@ -36,38 +38,41 @@ class MLP(Model):
         [-0.1, 0.1].
     """
     def __init__(self, di, dh, do, eps=0.1):
-        temp = dh / 2
-        self.w1_a = np.random.random((temp, di+1))*eps*2-eps
-        self.w1_b = np.random.random((temp, di+1))*eps*2-eps
+        temp = dh
+        self.w1 = np.random.random((temp, di+1))*eps*2-eps
+        # self.w1_b = np.random.random((temp, di+1))*eps*2-eps
         self.w2 = np.random.random((do, dh+1))*eps*2-eps
         self.costs = []
 
-    def get_activations(self, x1, x2):
-        self.yh_a = self.sigmoid(np.dot(self.w1_a, x1))
-        self.yh_b = self.sigmoid(np.dot(self.w1_b, x2))
-
-        self.yh = np.vstack((np.ones(self.bsize), self.yh_a, self.yh_b))
+    def get_activations(self, x):
+        # self.yh_a = self.sigmoid(np.dot(self.w1_a, x1))
+        # self.yh_b = self.sigmoid(np.dot(self.w1_b, x2))
+        self.yh = self.sigmoid(np.dot(self.w1, x))
+        # self.yh = np.vstack((np.ones(self.bsize), self.yh_a, self.yh_b))
+        self.yh = np.vstack((np.ones(self.bsize), self.yh))
 
         self.yo = self.softmax(np.dot(self.w2, self.yh))
 
-    def train(self, snli, iters, bsize=200, rate=0.25):
+    def train(self, snli, iters, bsize=200, rate=0.3):
         self.bsize = bsize
 
         BoW = CountVectorizer(binary=True)
         BoW.fit(snli.vocab)
 
         snli.extractor = snli.get_xy_pairs
-        data = {d for d in snli.dev_data}
+        data = {d for d in snli.train_data if d[1] != '-'}
+        print('Training set size: ', len(data))
 
         for _ in range(iters):
 
-            if _ % 1000 == 0:
-                print(_)
+            if _ % 10000 == 0:
+                if _ != 0:
+                    print('Completed ', _, ' training iterations!')
 
-            if _ == 0.7 * iters:
+            if _ == 0.6 * iters:
                 rate = 0.15
                 print('Dropped rate to ', rate)
-            if _ == 0.95 * iters:
+            if _ == 0.8 * iters:
                 rate = 0.075
                 print('Dropped rate to ', rate)
 
@@ -82,23 +87,27 @@ class MLP(Model):
             prem_bag = np.vstack((np.ones(self.bsize), prem_bag))
 
             hyp_bag = BoW.transform(hyps).toarray().T
-            hyp_bag = np.vstack((np.ones(self.bsize), hyp_bag))
+            # hyp_bag = np.vstack((np.ones(self.bsize), hyp_bag))
 
+            inp_bag = np.vstack((prem_bag, hyp_bag))
             self.targ_bag = self.binarize(targs)
 
             # Compute activations
-            self.get_activations(prem_bag, hyp_bag)
+            self.get_activations(inp_bag)
 
             # Compute gradients
             yo_grad = self.yo-self.targ_bag
             yh_grad = np.dot(self.w2.T, yo_grad)*(self.yh*(1-self.yh))
             w2_grad = np.dot(yo_grad, self.yh.T) / self.bsize
-            w1a_grad = np.dot(yh_grad[1:101, :], prem_bag.T) / self.bsize
-            w1b_grad = np.dot(yh_grad[101:, :], hyp_bag.T) / self.bsize
+            w1_grad = np.dot(yh_grad[1:, :], inp_bag.T) / self.bsize
+
+            # w1a_grad = np.dot(yh_grad[1:201, :], prem_bag.T) / self.bsize
+            # w1b_grad = np.dot(yh_grad[201:, :], hyp_bag.T) / self.bsize
 
             # Update weights
-            self.w1_a += -rate * w1a_grad
-            self.w1_b += -rate * w1b_grad
+            self.w1 += -rate * w1_grad
+            # self.w1_a += -rate * w1a_grad
+            # self.w1_b += -rate * w1b_grad
             self.w2 += -rate * w2_grad
 
             # Log the cost of the current weights
@@ -107,34 +116,61 @@ class MLP(Model):
     def get_cost(self):
         return np.sum(-np.log(self.yo) * self.targ_bag) / float(self.bsize)
 
-    def predict(self, x1, x2):
-        self.get_activations(x1, x2)
+    def predict(self, x):
+        self.get_activations(x)
         return np.argmax(self.yo, axis=0)
 
-    def get_accuracy(self, snli):
+    def get_accuracy(self, snli, dataset):
 
         snli.extractor = snli.get_xy_pairs
 
-        data = {d for d in snli.dev_data}
-        self.bsize = len(data)
+        def tally(data):
+            # Turn batches into arrays
+            self.bsize = len(data)
 
-        # Turn batches into arrays
-        prems = [s[0][0] for s in data]
-        hyps = [s[0][1] for s in data]
-        targs = [s[1].encode('ascii') for s in data]
+            prems = [s[0][0] for s in data]
+            hyps = [s[0][1] for s in data]
+            targs = [s[1].encode('ascii') for s in data]
 
-        prem_bag = BoW.transform(prems).toarray().T
-        prem_bag = np.vstack((np.ones(len(data)), prem_bag))
+            prem_bag = BoW.transform(prems).toarray().T
+            prem_bag = np.vstack((np.ones(len(data)), prem_bag))
 
-        hyp_bag = BoW.transform(hyps).toarray().T
-        hyp_bag = np.vstack((np.ones(len(data)), hyp_bag))
+            hyp_bag = BoW.transform(hyps).toarray().T
+            # hyp_bag = np.vstack((np.ones(self.bsize), hyp_bag))
 
-        targs = self.binarize(targs)
+            inp_bag = np.vstack((prem_bag, hyp_bag))
 
-        correct = sum(np.equal(self.predict(prem_bag, hyp_bag),
-                      np.argmax(targs, axis=0)))
+            targs = self.binarize(targs)
 
-        return 100 * (correct / float(self.bsize))
+            correct = sum(np.equal(self.predict(inp_bag),
+                          np.argmax(targs, axis=0)))
+
+            return correct
+
+        if dataset == 'dev':
+            data = {d for d in snli.dev_data if d[1] != '-'}
+            self.bsize = len(data)
+            return tally(data) / float(self.bsize)
+
+        if dataset == 'train':
+            correct = 0
+            bsize = 10000
+            for _ in range(55):
+                data = []
+                for __ in range(bsize):
+                    try:
+                        while True:
+                            x = next(snli.train_data)
+                            if x[1] != '-':
+                                break
+                        data.append(x)
+                    except:
+                        break
+                correct += tally(data)
+
+            snli.extractor = snli.get_xy_pairs
+            return correct / float(len({d for d in snli.train_data
+                                   if d[1] != '-'}))
 
     @staticmethod
     def binarize(label_list):
@@ -161,11 +197,14 @@ snli.load_vocab()
 BoW = CountVectorizer(binary=True)
 BoW.fit(snli.vocab)
 
-model = MLP(len(BoW.get_feature_names()), 200, 3)
-print('Before: ', model.get_accuracy(snli))
-snli._reset_streams()
+model = MLP(2*len(BoW.get_feature_names()), 400, 3)
+print('Dev Set Accuracy Before: ', model.get_accuracy(snli, 'dev'))
 
-model.train(snli, iters=50000, bsize=300)
-snli._reset_streams()
+model.train(snli, iters=25000, bsize=600)
 
-print('After: ', model.get_accuracy(snli))
+print('Train Set Accuracy After: ', model.get_accuracy(snli, 'train'))
+print('Dev Set Accuracy After: ', model.get_accuracy(snli, 'dev'))
+
+plt.figure()
+plt.plot(np.arange(len(model.costs)), model.costs)
+plt.show()
