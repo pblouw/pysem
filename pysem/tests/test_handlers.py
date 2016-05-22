1fa750cd7b7ce43c5a4f3f8c40bf2fd439221880
pblouw <pblouw@gmail.com>
2016-05-22 15:19:48 -0500

    Add test for writing to cache

--- pysem/tests/test_handlers.py
+++ pysem/tests/test_handlers.py
@@ -3,11 +3,12 @@ import re
 import types
 
 from pysem.handlers import Wikipedia
+from pysem.mputils import max_strip
 
-corpus_path = os.getcwd() + '/pysem/tests/corpora'
+corpus_path = os.getcwd() + '/pysem/tests/corpora/'
 
 
-def test_handler():
+def test_streaming():
     wp = Wikipedia(corpus_path)
 
     assert isinstance(wp.batches, types.GeneratorType)
@@ -19,7 +20,7 @@ def test_handler():
     assert isinstance(next(wp.sentences), str)
 
 
-def test_preprocess():
+def test_preprocessing():
     wp = Wikipedia(corpus_path)
     article = next(wp.articles)
 
@@ -28,3 +29,20 @@ def test_preprocess():
 
     pattern = re.compile(r"\n")
     assert not pattern.findall(article)
+
+
+def test_caching(tmpdir):
+    cache_path = str(tmpdir) + '/'
+
+    wp = Wikipedia(corpus_path)
+    wp.write_to_cache(cache_path, processor=max_strip, n_per_file=1)
+
+    wp_cache = Wikipedia(cache_path, from_cache=True)
+    article = next(wp_cache.articles)
+    assert isinstance(article, str)
+
+    pattern = re.compile(r"[0-9]|\[|\<|\(|\"|\'|\{")
+    assert not pattern.findall(article)
+
+    all_cached_articles = [a for a in wp_cache.articles]
+    assert len(all_cached_articles) < 90
