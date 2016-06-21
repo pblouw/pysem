16acb5242e9b87c7f7e9fc04ef206169b0830acf
pblouw <pblouw@uwaterloo.ca>
2016-06-20 20:55:07 -0400

    Add setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pysem',
+      version='0.1',
+      description='NLP with distributed representations in python',
+      url='http://github.com/pblouw/pysem',
+      author='Peter Blouw',
+      author_email='pblouw@uwaterloo.ca',
+      packages=['pysem'])
