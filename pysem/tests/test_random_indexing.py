ecabf10da423bbf3d98681e6505e44e91a544b14
pblouw <pblouw@uwaterloo.ca>
2016-06-26 20:08:58 -0400

    Add tests for static methods used in pools

--- pysem/tests/test_random_indexing.py
+++ pysem/tests/test_random_indexing.py
@@ -1,6 +1,8 @@
 import os
+import random
 import numpy as np
 
+from collections import defaultdict
 from pysem.corpora import Wikipedia
 from pysem.embeddings import ContextEmbedding, OrderEmbedding, SyntaxEmbedding
 
@@ -16,6 +18,15 @@ def test_context_embedding():
 
     assert isinstance(model.vectors, np.ndarray)
 
+    model.corpus.reset_streams()
+    sen_list = model.preprocess(next(model.corpus.articles))
+    encoding = model.encode(sen_list)
+
+    assert isinstance(sen_list, list)
+    assert isinstance(''.join(random.choice(sen_list)), str)
+    assert isinstance(encoding, defaultdict)
+    assert isinstance(encoding[wp.vocab[0]], np.ndarray)
+
 
 def test_order_embedding():
     wp = Wikipedia(corpus_path, article_limit=1)
@@ -26,6 +37,13 @@ def test_order_embedding():
 
     assert isinstance(model.vectors, np.ndarray)
 
+    model.corpus.reset_streams()
+    sen_list = model.preprocess(next(model.corpus.articles))
+    encoding = model.encode(sen_list)
+
+    assert isinstance(encoding, defaultdict)
+    assert isinstance(encoding[wp.vocab[0]], np.ndarray)
+
 
 def test_syntax_embedding():
     wp = Wikipedia(corpus_path, article_limit=1)
@@ -35,3 +53,9 @@ def test_syntax_embedding():
     model.train(dim=16, batchsize=1)
 
     assert isinstance(model.vectors, np.ndarray)
+
+    model.corpus.reset_streams()
+    encoding = model.encode(next(model.corpus.articles))
+
+    assert isinstance(encoding, defaultdict)
+    assert isinstance(encoding[wp.vocab[0]], np.ndarray)
