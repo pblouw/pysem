d56f9384a4c21fd567493322550b66834efa6652
pblouw <pblouw@uwaterloo.ca>
2016-06-29 15:25:04 -0400

    Add tests for backprop in depnet

--- pysem/tests/test_dependency_network.py
+++ pysem/tests/test_dependency_network.py
@@ -24,6 +24,7 @@ def test_token_wrapper():
     depnet.forward_pass(sen)
 
     node = random.choice(depnet.tree)
+
     with pytest.raises(TypeError):
         node.embedding = ''
         node.gradient = ''
@@ -54,3 +55,39 @@ def test_forward_pass():
 
     for node in depnet.tree:
         assert isinstance(node.embedding, np.ndarray)
+
+
+def test_backward_pass():
+    snli = SNLI(snli_path)
+    snli.build_vocab()
+    snli.extractor = snli.get_sentences
+
+    dim = 50
+    eps = 0.1
+
+    depnet = DependencyNetwork(dim=dim, vocab=snli.vocab)
+    sample = next(snli.train_data)
+    sen = random.choice(sample)
+
+    error_grad = np.random.random((dim, 1)) * 2 * eps - eps
+
+    depnet.forward_pass(sen)
+    deps = [node.dep_ for node in depnet.tree]
+
+    weights = []
+    for dep in deps:
+        weights.append(depnet.weights[dep])
+
+    depnet.backward_pass(error_grad, rate=0.1)
+
+    for node in depnet.tree:
+        assert isinstance(node.gradient, np.ndarray)
+
+    new_weights = []
+    for dep in deps:
+        new_weights.append(depnet.weights[dep])
+
+    differences = [w[1]-w[0] for w in zip(weights, new_weights)]
+
+    for difference in differences:
+        assert np.count_nonzero(difference) == 0
