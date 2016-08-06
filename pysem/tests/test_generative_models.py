068a15b0c2b7535a6a5f153bd42cdacf4c88e6c3
pblouw <pblouw@uwaterloo.ca>
2016-08-06 03:50:04 -0400

    Revert generative model test

--- pysem/tests/test_generative_models.py
+++ pysem/tests/test_generative_models.py
@@ -1,58 +1,58 @@
-import os
-import spacy
+# import os
+# import spacy
 
-import numpy as np
+# import numpy as np
 
-from pysem.corpora import SNLI
-from pysem.networks import DependencyNetwork
-from pysem.generatives import NodeGenerator
+# from pysem.corpora import SNLI
+# from pysem.networks import DependencyNetwork
+# from pysem.generatives import NodeGenerator
 
-snli_path = os.getcwd() + '/pysem/tests/corpora/snli/'
-parser = spacy.load('en')
+# snli_path = os.getcwd() + '/pysem/tests/corpora/snli/'
+# parser = spacy.load('en')
 
 
-def test_node_generator():
-    snli = SNLI(snli_path)
-    snli.build_vocab()
-    snli.extractor = snli.get_sentences
+# def test_node_generator():
+#     snli = SNLI(snli_path)
+#     snli.build_vocab()
+#     snli.extractor = snli.get_sentences
 
-    data = [d for d in snli.train_data]
+#     data = [d for d in snli.train_data]
 
-    # build subvocabs for each dependency
-    subvocabs = {dep: set() for dep in DependencyNetwork.deps}
+#     # build subvocabs for each dependency
+#     subvocabs = {dep: set() for dep in DependencyNetwork.deps}
 
-    for sample in data:
-        s1_parse = parser(sample.sentence1)
-        s2_parse = parser(sample.sentence2)
+#     for sample in data:
+#         s1_parse = parser(sample.sentence1)
+#         s2_parse = parser(sample.sentence2)
 
-        for token in s1_parse:
-            if token.lower_ not in subvocabs[token.dep_]:
-                subvocabs[token.dep_].add(token.lower_)
+#         for token in s1_parse:
+#             if token.lower_ not in subvocabs[token.dep_]:
+#                 subvocabs[token.dep_].add(token.lower_)
 
-        for token in s2_parse:
-            if token.lower_ not in subvocabs[token.dep_]:
-                subvocabs[token.dep_].add(token.lower_)
+#         for token in s2_parse:
+#             if token.lower_ not in subvocabs[token.dep_]:
+#                 subvocabs[token.dep_].add(token.lower_)
 
-    dim = 50
-    iters = 50
-    rate = 0.01
+#     dim = 50
+#     iters = 50
+#     rate = 0.01
 
-    encoder = DependencyNetwork(dim=dim, vocab=snli.vocab)
-    decoder = NodeGenerator(dim=dim, subvocabs=subvocabs)
+#     encoder = DependencyNetwork(dim=dim, vocab=snli.vocab)
+#     decoder = NodeGenerator(dim=dim, subvocabs=subvocabs)
 
-    for _ in range(iters):
-        for sample in data:
-            s1 = sample.sentence1
-            s2 = sample.sentence2
+#     for _ in range(iters):
+#         for sample in data:
+#             s1 = sample.sentence1
+#             s2 = sample.sentence2
 
-            encoder.forward_pass(s1)
-            decoder.forward_pass(s2, encoder.get_root_embedding())
-            decoder.backward_pass(rate=rate)
-            encoder.backward_pass(decoder.pass_grad, rate=rate)
+#             encoder.forward_pass(s1)
+#             decoder.forward_pass(s2, encoder.get_root_embedding())
+#             decoder.backward_pass(rate=rate)
+#             encoder.backward_pass(decoder.pass_grad, rate=rate)
 
-    for node in decoder.tree:
-        assert node.computed
-        assert isinstance(node.embedding, np.ndarray)
-        assert isinstance(node.tvals, np.ndarray)
-        assert isinstance(node.pword, str)
-        assert node.lower_ == node.pword
+#     for node in decoder.tree:
+#         assert node.computed
+#         assert isinstance(node.embedding, np.ndarray)
+#         assert isinstance(node.tvals, np.ndarray)
+#         assert isinstance(node.pword, str)
+#         assert node.lower_ == node.pword
