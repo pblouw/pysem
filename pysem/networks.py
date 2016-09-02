Simplify RNN tests and fix input gradients

pysem/networks.py
@@ -7,6 +7,7 @@ import numpy as np
 from collections import defaultdict
 from pysem.utils.spacy import TokenWrapper
 from pysem.utils.vsa import normalize, unitary_vector, get_convolution_matrix
+from pysem.utils.multiprocessing import flatten
 
 
 def square_zeros(dim):
@@ -165,7 +166,6 @@ class RecurrentNetwork(RecursiveModel):
         self.by = np.zeros((dim, 1))
 
         self.pretrained_vecs(pretrained) if pretrained else self.random_vecs()
-        self.dwrd = {}
 
     def clip_gradient(self, gradient, clipval=5):
         '''Clip a large gradient so that its norm is equal to clipval.'''
@@ -214,6 +214,7 @@ class RecurrentNetwork(RecursiveModel):
         '''Compute gradients for hidden-to-hidden weight matrix and input word
         vectors before performing weight updates.'''
         error_grad = error_grad * self.tanh_grad(self.get_root_embedding())
+        self.xgrads = defaultdict(flat_zeros(self.dim))
 
         dwhh = np.zeros_like(self.whh)
         dbh = np.zeros_like(self.bh)
@@ -238,8 +239,7 @@ class RecurrentNetwork(RecursiveModel):
                 if word != 'PAD':
                     try:
                         item = word.lower()
-                        self.dwrd[item] = dh[:, idx].reshape(self.dim, 1)
-                        self.vectors[item] -= rate * self.dwrd[item]
+                        self.xgrads[item] += dh[:, idx].reshape(self.dim, 1)
                     except KeyError:
                         pass
 
@@ -247,11 +247,19 @@ class RecurrentNetwork(RecursiveModel):
         self.dwhh = self.clip_gradient(dwhh / self.bsize)
         self.dbh = self.clip_gradient(dbh / self.bsize)
         self.dby = self.clip_gradient(dby / self.bsize)
+        self.xgrads = {w: g / self.bsize for w, g in self.xgrads.items()}
 
         self.why -= rate * self.dwhy
         self.whh -= rate * self.dwhh
         self.bh -= rate * self.dbh
         self.by -= rate * self.dby
+
+        word_set = [w.lower() for w in set(flatten(self.batch)) if w != 'PAD']
+        all_words = [w.lower() for w in flatten(self.batch) if w != 'PAD']
+
+        for word in word_set:
+            count = all_words.count(word)
+            self.vectors[word] -= rate * self.xgrads[word] / count
 
     def get_root_embedding(self):
         '''Returns the embeddings for the final/root node in the sequence.'''

pysem/tests/test_recurrent_network.py
@@ -1,134 +1,85 @@
-import os
-import string
+import random
 
 import numpy as np
 
-from pysem.utils.ml import LogisticRegression
-
-strip_pun = str.maketrans({key: None for key in string.punctuation})
-snli_path = os.getcwd() + '/pysem/tests/corpora/snli/'
-
-
-def get_cost(model, logreg, xs, ys):
-    model.forward_pass(xs)
-    embedding = model.get_root_embedding()
-
-    return logreg.get_cost(embedding, ys)
+n_gradient_checks = 25
+n_labels = 3
+bsize = 10
+rate = 0.001
 
 
-def num_grad(model, params, idx, xs, ys, logreg, delta=1e-5):
-    val = np.copy(params[idx])
+def random_data(snli):
+    samples = [random.choice(snli.data) for _ in range(bsize)]
+    xs = [random.choice(sample) for sample in samples]
+    ys = np.zeros((n_labels, bsize))
+    ys[np.random.randint(0, n_labels, bsize), list(range(bsize))] = 1
 
-    params[idx] = val + delta
-    pcost = get_cost(model, logreg, xs, ys)
+    return xs, ys
 
-    params[idx] = val - delta
-    ncost = get_cost(model, logreg, xs, ys)
 
-    params[idx] = val
-    numerical_gradient = (pcost - ncost) / (2 * delta)
-
-    return numerical_gradient
+def train_step(s_model, classifier, xs, ys):
+    s_model.forward_pass(xs)
+    classifier.train(s_model.get_root_embedding(), ys, rate=rate)
+    s_model.backward_pass(classifier.yi_grad, rate=rate)
 
 
 def test_forward_pass(rnn, snli):
-    sens = [next(snli.train_data) for _ in range(5)]
-    sens = [item.sentence1 for item in sens]
+    xs, ys = random_data(snli)
+    rnn.forward_pass(xs)
 
-    rnn.forward_pass(sens)
-    sen_vec = rnn.get_root_embedding()
-
-    assert isinstance(sen_vec, np.ndarray)
+    assert isinstance(rnn.get_root_embedding(), np.ndarray)
 
 
 def test_backward_pass(rnn, snli):
-    dim = 50
-    eps = 0.5
+    xs, ys = random_data(snli)
 
-    # rnn = RecurrentNetwork(dim=dim, vocab=snli.vocab)
-    sens = [next(snli.train_data) for _ in range(5)]
-    sens = [item.sentence1 for item in sens]
+    error_grad = np.random.random((rnn.dim, bsize)) * 2 * 0.5 - 0.5
 
-    error_grad = np.random.random((dim, 5)) * 2 * eps - eps
-
-    rnn.forward_pass(sens)
+    rnn.forward_pass(xs)
 
     # Save a copy of the weights before SGD update
     weights = np.copy(rnn.whh)
 
     # Do backprop
-    rnn.backward_pass(error_grad, rate=0.1)
+    rnn.backward_pass(error_grad, rate=rate)
 
+    # Save a copy of the weights after SGD update
     new_weights = np.copy(rnn.whh)
 
-    # Check that every weight has changed after the SGD update
+    # Check that every weight has changed across the update
     assert np.count_nonzero(weights - new_weights) == weights.size
 
 
-def test_weight_gradients(rnn, snli):
-    dim = 50
-    n_labels = 3
-    n_gradient_checks = 25
-
-    logreg = LogisticRegression(n_features=dim, n_labels=n_labels)
-
-    xs = [next(snli.train_data) for _ in range(5)]
-    xs = [item.sentence1 for item in xs]
-    ys = np.zeros((n_labels, 5))
-    ys[np.random.randint(0, n_labels, 5), list(range(5))] = 1
-
-    rnn.forward_pass(xs)
+def test_weight_gradients(rnn, snli, get_cost, num_grad, classifier):
+    xs, ys = random_data(snli)
 
     # Use random weight in each matrix for n numerical gradient checks
     for _ in range(n_gradient_checks):
         idx = np.random.randint(0, rnn.whh.size, size=1)
         params = rnn.whh.flat
 
-        numerical = num_grad(rnn, params, idx, xs, ys, logreg)
+        numerical_grad = num_grad(rnn, params, idx, xs, ys, classifier)
+        train_step(rnn, classifier, xs, ys)
+        analytic_grad = rnn.dwhh.flat[idx]
 
-        rnn.forward_pass(xs)
-
-        logreg.train(rnn.get_root_embedding(), ys, rate=0.001)
-
-        rnn.backward_pass(logreg.yi_grad, rate=0.001)
-        analytic = rnn.dwhh.flat[idx]
-
-        assert np.allclose(analytic, numerical)
+        assert np.allclose(analytic_grad, numerical_grad)
 
         idx = np.random.randint(0, rnn.why.size, size=1)
         params = rnn.why.flat
 
-        numerical = num_grad(rnn, params, idx, xs, ys, logreg)
+        numerical_grad = num_grad(rnn, params, idx, xs, ys, classifier)
+        train_step(rnn, classifier, xs, ys)
+        analytic_grad = rnn.dwhy.flat[idx]
 
-        rnn.forward_pass(xs)
-
-        logreg.train(rnn.get_root_embedding(), ys, rate=0.001)
-
-        rnn.backward_pass(logreg.yi_grad, rate=0.001)
-        analytic = rnn.dwhy.flat[idx]
-
-        assert np.allclose(analytic, numerical)
+        assert np.allclose(analytic_grad, numerical_grad)
 
 
-def test_embedding_gradients(rnn, snli):
-    snli.reset_streams()
-    snli.extractor = snli.get_sentences
-
-    dim = 50
-    n_labels = 3
-    n_gradient_checks = 25
-
-    logreg = LogisticRegression(n_features=dim, n_labels=n_labels)
-
-    xs = [next(snli.train_data) for _ in range(1)]
-    xs = [item.sentence1 for item in xs]
-    ys = np.zeros((n_labels, 1))
-    ys[np.random.randint(0, n_labels, 1), list(range(1))] = 1
+def test_embedding_gradients(rnn, snli, get_cost, num_grad, classifier):
+    xs, ys = random_data(snli)
 
     rnn.forward_pass(xs)
-    words = xs[0].split()
-    words = [word.translate(strip_pun) for word in words]
-    words = [word.lower() for word in words]
+    words = rnn.batch[0]
+    words = [w.lower() for w in words if w != 'PAD']
 
     # Use random element in each word embedding for n numerical gradient checks
     for _ in range(n_gradient_checks):
@@ -137,58 +88,32 @@ def test_embedding_gradients(rnn, snli):
             idx = np.random.randint(0, rnn.vectors[word].size, size=1)
             params = rnn.vectors[word].flat
 
-            numerical = num_grad(rnn, params, idx, xs, ys, logreg)
+            numerical_grad = num_grad(rnn, params, idx, xs, ys, classifier)
+            train_step(rnn, classifier, xs, ys)
+            analytic_grad = rnn.xgrads[word].flat[idx]
 
-            rnn.forward_pass(xs)
-
-            logreg.train(rnn.get_root_embedding(), ys, rate=0.001)
-
-            rnn.backward_pass(logreg.yi_grad, rate=0.001)
-            analytic = rnn.dwrd[word].flat[idx]
-
-            assert np.allclose(analytic, numerical)
+            assert np.allclose(analytic_grad, numerical_grad)
 
 
-def test_bias_gradients(rnn, snli):
-    dim = 50
-    n_labels = 3
-    n_gradient_checks = 25
+def test_bias_gradients(rnn, snli, get_cost, num_grad, classifier):
+    xs, ys = random_data(snli)
 
-    logreg = LogisticRegression(n_features=dim, n_labels=n_labels)
-
-    xs = [next(snli.train_data) for _ in range(5)]
-    xs = [item.sentence1 for item in xs]
-    ys = np.zeros((n_labels, 5))
-    ys[np.random.randint(0, n_labels, 5), list(range(5))] = 1
-
-    rnn.forward_pass(xs)
-
-    # Use random weight in each matrix for n numerical gradient checks
+    # Use random element in each bias vector for n numerical gradient checks
     for _ in range(n_gradient_checks):
         idx = np.random.randint(0, rnn.bh.size, size=1)
         params = rnn.bh.flat
 
-        numerical = num_grad(rnn, params, idx, xs, ys, logreg)
+        numerical_grad = num_grad(rnn, params, idx, xs, ys, classifier)
+        train_step(rnn, classifier, xs, ys)
+        analytic_grad = rnn.dbh.flat[idx]
 
-        rnn.forward_pass(xs)
-
-        logreg.train(rnn.get_root_embedding(), ys, rate=0.001)
-
-        rnn.backward_pass(logreg.yi_grad, rate=0.001)
-        analytic = rnn.dbh.flat[idx]
-
-        assert np.allclose(analytic, numerical)
+        assert np.allclose(analytic_grad, numerical_grad)
 
         idx = np.random.randint(0, rnn.by.size, size=1)
         params = rnn.by.flat
 
-        numerical = num_grad(rnn, params, idx, xs, ys, logreg)
+        numerical_grad = num_grad(rnn, params, idx, xs, ys, classifier)
+        train_step(rnn, classifier, xs, ys)
+        analytic_grad = rnn.dby.flat[idx]
 
-        rnn.forward_pass(xs)
-
-        logreg.train(rnn.get_root_embedding(), ys, rate=0.001)
-
-        rnn.backward_pass(logreg.yi_grad, rate=0.001)
-        analytic = rnn.dby.flat[idx]
-
-        assert np.allclose(analytic, numerical)
+        assert np.allclose(analytic_grad, numerical_grad)