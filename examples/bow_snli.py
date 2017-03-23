8a78d5514cc62ab0c322c87ef8dafce2d3d8b76c
pblouw <pblouw@uwaterloo.ca>
2017-03-23 17:34:10 -0400

    Update SNLI evaluation scripts

--- examples/bow_snli.py
+++ examples/bow_snli.py
@@ -2,9 +2,9 @@ import pickle
 import time
 
 from pysem.corpora import SNLI
-from pysem.networks import RecurrentNetwork
+from pysem.networks import DependencyNetwork
 from pysem.utils.ml import MultiLayerPerceptron
-from pysem.utils.snli import CompositeModel
+from pysem.utils.snli import CompositeModel, BagOfWords
 
 snli = SNLI('/Users/peterblouw/corpora/snli_1.0/')
 snli.load_vocab('snli_words.pickle')
@@ -12,20 +12,19 @@ snli.load_vocab('snli_words.pickle')
 dim = 300
 pretrained = 'pretrained_snli_embeddings.pickle'
 
-encoder = RecurrentNetwork(dim=dim, vocab=snli.vocab, pretrained=pretrained)
+encoder = BagOfWords(dim=dim, vocab=snli.vocab, pretrained=pretrained)
 classifier = MultiLayerPerceptron(di=2*dim, dh=dim, do=3)
 
-model = CompositeModel(snli, encoder, classifier)
-
 start_time = time.time()
+model = CompositeModel(snli, encoder, classifier)
+model.train(iters=50, bsize=100, rate=0.1, log_interval=10, schedule=12000)
 
-model.train(iters=100000, bsize=100, rate=0.1, log_interval=10000, schedule=15000)
 print('Test: ', model.rnn_accuracy(model.test_data))
 print('Train: ', model.rnn_accuracy(model.train_data))
 print('Dev: ', model.rnn_accuracy(model.dev_data))
 
-print('Total runtime: ', ((time.time() - start_time) / 3600.0))
+print('Total runtime: ', time.time() - start_time)
 
-with open('rnn_model_w2v_high_learning_rate.pickle', 'wb') as pfile:
+with open('bow_model', 'wb') as pfile:
     pickle.dump(model, pfile)
 
--- examples/dnn_snli.py
+++ examples/dnn_snli.py
@@ -1,17 +1,13 @@
-import platform
-import random
 import time
-import sys
 import pickle
-import numpy as np
 
 from pysem.corpora import SNLI
 from pysem.networks import DependencyNetwork
 from pysem.utils.ml import MultiLayerPerceptron
 from pysem.utils.snli import CompositeModel
-from itertools import islice
 
-snli = SNLI('/home/pblouw/snli_1.0/')
+
+snli = SNLI('/Users/peterblouw/corpora/snli_1.0/')
 snli.load_vocab('snli_words.pickle')
 
 dim = 300
@@ -24,19 +20,5 @@ model = CompositeModel(snli, encoder, classifier)
 
 start_time = time.time()
 
-model = CompositeModel(snli, encoder, classifier)
+model.train(iters=1, bsize=1, rate=0.005, log_interval=2, schedule=12000)
 
-model.train(iters=50000, bsize=100, rate=0.005, log_interval=1000, schedule=12000)
-print(model.dnn_accuracy(model.test_data))
-model.plot()
-
-print('Total runtime: ', time.time() - start_time)
-
-with open('dnn_model', 'wb') as pfile:
-	pickle.dump(model, pfile)
-
-with open('dnn_model', 'rb') as pfile:
-	test_model = pickle.load(pfile)
-
-print('Testing Model Loading')
-print(test_model.dnn_accuracy(test_model.test_data))
